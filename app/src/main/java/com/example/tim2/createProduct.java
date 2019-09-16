0030deec534a60e53e4335ceb6d0d4c11daf1cb2
Paulina <1355485@students.wits.ac.za>
2019-09-16 15:21:26 +0200

    Create Product CheckBoxes working

--- app/src/main/java/com/example/tim2/createProduct.java
+++ app/src/main/java/com/example/tim2/createProduct.java
@@ -5,9 +5,13 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -15,7 +19,6 @@ import org.json.JSONObject;
 
 public class createProduct extends AppCompatActivity {
     String username;
-    CheckBox c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,12 +33,27 @@ public class createProduct extends AppCompatActivity {
         cv.put("shopName","Caves");
 
         LinearLayout holder = findViewById(R.id.productHolder);
-        c = findViewById(R.id.checkBox);
+        EditText productName = findViewById(R.id.productName_editText);
+        Button createProduct = findViewById(R.id.btnCreateProduct);
+
+        createProduct.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         displayItems(holder,cv);
 
-
     }
+
+    public void onCheckBox(View view, CheckBox c){
+
+        if(c.isChecked()){
+            Toast.makeText(createProduct.this, "Checked", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 
     public void displayItems(final LinearLayout holder, ContentValues cv) {
 
@@ -52,6 +70,17 @@ public class createProduct extends AppCompatActivity {
 
                         ((TextView) v.findViewById(R.id.itemShown)).setText("Name: "+ shop.getString("itemName") + "\n" +"Desc: "+ shop.get("itemDescription") + "\n" +"Quantity:" + shop.get("itemQuantity"));
                         System.out.println("working");
+
+                       final CheckBox  c = v.findViewById(R.id.checkBox);
+
+                        c.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+
+                                onCheckBox(v,c);
+                            }
+                        });
+
 
                         holder.addView(v);
                     }

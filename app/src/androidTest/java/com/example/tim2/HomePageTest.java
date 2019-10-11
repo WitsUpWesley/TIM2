55e14fed61f6f91b0632be08cd44fde4fdd107a6
WitsUpSteven <1657041@students.wits.ac.za>
2019-10-11 19:47:14 +0200

    Added HomePage Database Tests

--- app/src/androidTest/java/com/example/tim2/HomePageTest.java
+++ app/src/androidTest/java/com/example/tim2/HomePageTest.java
@@ -2,16 +2,22 @@ package com.example.tim2;
 
 import android.app.Activity;
 import android.app.Instrumentation;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.InstrumentationRegistry.getContext;
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
@@ -91,6 +97,19 @@ public class HomePageTest {
         secondAct.finish();
 
 
+    }
+
+    @Test
+    public void database() throws Exception {
+        ContentValues cv = new ContentValues();
+        cv.put("owner", "a");
+
+        new AsyncHttpPost("http://lamp.ms.wits.ac.za/~s1355485/getShopFromOwner.php", cv) {
+            @Override
+            protected void onPostExecute(String output) {
+                System.out.println(output);
+            }
+        }.execute();
     }
 
 }

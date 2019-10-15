85f20a72fd4d2daeb71ee629016d469f65b9c9fe
WitsUpSteven <1657041@students.wits.ac.za>
2019-10-15 11:05:04 +0200

    added Tests

---
+++ app/src/androidTest/java/com/example/tim2/DeleteItemTest.java
@@ -0,0 +1,38 @@
+package com.example.tim2;
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.InstrumentationRegistry.getInstrumentation;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+public class DeleteItemTest {
+    @Rule
+    public ActivityTestRule rule=new ActivityTestRule(DeleteItem.class,true,false);
+
+    String username="a";
+    String storeName="Toys Were Us";
+    @Test
+    public void checkText() throws Exception {
+        rule.launchActivity(new Intent().putExtra(username, "a").putExtra(storeName, "Toys Were Us"));
+        onView(withId(R.id.btnDelete)).check(matches(withText("DELETE")));
+    }
+}
